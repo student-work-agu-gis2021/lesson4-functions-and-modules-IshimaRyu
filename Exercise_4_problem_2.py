9104ac2c62b2b980d2f0c14d96a2b279d18b3c3c
IshimaRyu <>
2021-05-03 01:55:42 +0000

    I printed answer 1 and 2.

--- Exercise_4_problem_2.py
+++ Exercise_4_problem_2.py
@@ -43,10 +43,13 @@ def temp_classifier(temp_celsius):
 
 # 1. What is the class value for 16.5 degrees Celsius?
 # YOUR CODE HERE
-#
+print(temp_classifier(16.5))
+# 3
+
 # 2. What is the class value for +2 degrees Celsius?
 # YOUR CODE HERE
-#
+print(temp_classifier(2))
+# 2
 
 
 # ### Problem 2 tests

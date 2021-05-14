00675c0ce4f3eb3be5f79a69889ed8bc7243a659
IshimaRyu <>
2021-05-14 01:59:31 +0000

    I added docstring.

--- temp_functions.py
+++ temp_functions.py
@@ -1,11 +1,11 @@
-"""今まで作ってきた関数を一つのファイルに入れた"""
+"""I put the functions I have created so far in one file"""
 def fahr_to_celsius(temp_fahrenheit):
-  """華氏をtemp_fahrenheitに代入すると摂氏に変化して、その値を返す"""
+  """Substituting Fahrenheit for temp_fahrenheit changes to Celsius and returns that value"""
   converted_temp=( temp_fahrenheit - 32 ) / 1.8
   return converted_temp
   
 def temp_classifier(temp_celsius):
-  """クラス分けしたい摂氏をtemp_celsiusに代入すると、その温度を4つのクラスのどこかに分ける"""
+  """Substituting the Celsius you want to classify into temp_celsius and devide the temperature into some of the four classes"""
   if temp_celsius<-2:
     return 0
   elif temp_celsius>=-2 and temp_celsius<2:
